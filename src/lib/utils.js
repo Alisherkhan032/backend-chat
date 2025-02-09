270805329273de76d29fb629b0ff93e3a4f1b470
alisherkhann <sherkhanali032@gmail.com>
2025-02-10 02:28:51 +0530

    bug fixed

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (userId, res)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn : '7d'
-    })
+const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '7d'
+    });
 
     res.cookie('token', token, {
-        maxAge : 7 * 24 * 60 * 60 * 1000, // 7 days
-        httpOnly : true, // prevent XSS attack
-        sameSite : true, // prevent CSRF attack
-        secure : process.env.NODE_ENV === 'production' ? true : false // only set cookie in https in production
-    })
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true, // Prevent XSS attacks
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Allow cross-site cookies
+        secure: process.env.NODE_ENV === 'production', // Only set cookie over HTTPS in production
+    });
 
     return token;
-}
+};
 
-module.exports = {generateToken};
+module.exports = { generateToken };
