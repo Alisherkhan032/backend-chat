b9c7cb843c31d84b8a0597ef003df71d876aaa9b
alisherkhann <sherkhanali032@gmail.com>
2025-02-09 20:50:45 +0530

    fix build bug

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ const cors = require('cors');
 const authRoutes = require("./routes/auth.route");
 const messageRoutes = require("./routes/message.route");
 const {app, server} = require("./lib/socket");
-const path = require("path");
+
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -22,14 +21,6 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend-chat/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend-chat/dist/index.html'));
-  });
- 
-}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
